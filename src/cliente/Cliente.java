aad7a9ae95c52608382940e70240eb0d5a65dd66
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2017-10-14 12:54:53 -0300

    commit

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,5 +1,35 @@
 package cliente;
-
+ 
+import java.io.*;
+import java.net.*;
+ 
 public class Cliente {
-
+ 
+    private static String HOST = "localhost";
+    private static int PUERTO = 2017;
+ 
+    public static void main(String args[]) {
+         
+        Socket socket;
+        DataOutputStream mensaje;
+         
+        try {
+            //Creamos nuestro socket
+            socket = new Socket(HOST, PUERTO);
+     
+            mensaje = new DataOutputStream(socket.getOutputStream());
+ 
+            //Enviamos un mensaje
+            mensaje.writeUTF("Hola soy un cliente!!");
+ 
+            //Cerramos la conexión
+            socket.close();
+ 
+        } catch (UnknownHostException e) {
+            System.out.println("El host no existe o no está activo.");
+        } catch (IOException e) {
+            System.out.println("Error de entrada/salida.");
+        }
+         
+    }
 }
